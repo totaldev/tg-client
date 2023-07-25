6516c55c539a64ae28283a4eb9cde17702d5695d
vk <4ws@mail.ru>
2023-07-26 01:16:43 +0600

    added verbose level IDE reformat code

--- src/TdLibFfiAdapter.php
+++ src/TdLibFfiAdapter.php
@@ -20,18 +20,19 @@ void td_json_client_send(void *client, const char *request);
 const char *td_json_client_receive(void *client, double timeout);
 const char *td_json_client_execute(void *client, const char *request);
 void td_json_client_destroy(void *client);
+void td_set_log_verbosity_level(int new_verbosity_level);
 HEADER;
 
-    private FFI $ffi;
-
     private FFI\CData $client;
+
+    private FFI $ffi;
 
     /**
      * @param string|null $libFile An optional file path/name to `libtdjson.so` library
      *
      * @throws AdapterException
      */
-    public function __construct(string $libFile = null)
+    public function __construct(string $libFile = null, int $logLevel = 0)
     {
         $libFile = $libFile ?? $this->getLibFilename();
 
@@ -40,22 +41,7 @@ HEADER;
         } catch (FFI\Exception $exception) {
             throw new AdapterException(sprintf('Failed loading TdLib library "%s"', $libFile));
         }
-    }
-
-    private function getLibFilename(): string
-    {
-        switch (PHP_OS_FAMILY) {
-            case 'Darwin':
-                return 'libtdjson.dylib';
-
-            case 'Windows':
-                return 'tdjson.dll';
-
-            case 'Linux':
-                return 'libtdjson.so';
-            default:
-                throw new AdapterException('Please specify tdjson library file');
-        }
+        $this->setVerbosityLevel($logLevel);
     }
 
     public function __destruct()
@@ -63,6 +49,37 @@ HEADER;
         if (isset($this->client)) {
             $this->ffi->td_json_client_destroy($this->client);
         }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute($request): ?array
+    {
+        $json = json_encode($request, JSON_THROW_ON_ERROR);
+
+        $response = $this->ffi->td_json_client_execute(null, $json);
+
+        if (null === $response) {
+            return null;
+        }
+
+        try {
+            return json_decode($response, true, JSON_THROW_ON_ERROR);
+        } catch (PHPJsonException $e) {
+            throw new JsonException($e->getMessage());
+        }
+    }
+
+    public function getClient(): FFI\CData
+    {
+        if (isset($this->client)) {
+            return $this->client;
+        }
+
+        $this->client = $this->ffi->td_json_client_create();
+
+        return $this->client;
     }
 
     /**
@@ -83,17 +100,6 @@ HEADER;
         }
     }
 
-    private function getClient(): FFI\CData
-    {
-        if (isset($this->client)) {
-            return $this->client;
-        }
-
-        $this->client = $this->ffi->td_json_client_create();
-
-        return $this->client;
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -108,23 +114,24 @@ HEADER;
         $this->ffi->td_json_client_send($this->getClient(), $json);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function execute($request): ?array
+    public function setVerbosityLevel(int $logLevel)
     {
-        $json = json_encode($request, JSON_THROW_ON_ERROR);
+        $this->ffi->td_set_log_verbosity_level($logLevel);
+    }
 
-        $response = $this->ffi->td_json_client_execute(null, $json);
+    private function getLibFilename(): string
+    {
+        switch (PHP_OS_FAMILY) {
+            case 'Darwin':
+                return 'libtdjson.dylib';
 
-        if (null === $response) {
-            return null;
-        }
+            case 'Windows':
+                return 'tdjson.dll';
 
-        try {
-            return json_decode($response, true, JSON_THROW_ON_ERROR);
-        } catch (PHPJsonException $e) {
-            throw new JsonException($e->getMessage());
+            case 'Linux':
+                return 'libtdjson.so';
+            default:
+                throw new AdapterException('Please specify tdjson library file');
         }
     }
 }
